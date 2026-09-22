57f12b516979bee0850653088742f791240a3bcd
manikanta <manikanta.n@yira.ai>
2026-09-22 22:17:37 +0530

    issues soved

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import path from 'path';
 // ClinicX Backend Application
 const app: Application = express();
 
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
